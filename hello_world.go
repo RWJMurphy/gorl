9ae10df7e0ca2d390e2887b3becc534bcbc8bb9f
Reed Kraft-Murphy <reed@reedmurphy.net>
2014-10-27 23:55:45 +1100

    Border!

--- hello_world.go
+++ hello_world.go
@@ -31,8 +31,30 @@ func (ui UI) PrintCentered(s string) {
 	ui.PrintAt(mid_w-s_len/2, mid_h, s)
 }
 
+func (ui UI) DrawVerticalLine(x, y1, y2 int, r rune) {
+	for y := y1; y <= y2; y++ {
+		termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
+	}
+}
+
+func (ui UI) DrawHorizontalLine(x1, y, x2 int, r rune) {
+	for x := x1; x <= x2; x++ {
+		termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
+	}
+}
+
+func (ui UI) DrawRectangle(x1, y1, x2, y2 int, r rune) {
+	ui.DrawHorizontalLine(x1, y1, x2, r)
+	ui.DrawHorizontalLine(x1, y2, x2, r)
+	ui.DrawVerticalLine(x1, y1, y2, r)
+	ui.DrawVerticalLine(x2, y1, y2, r)
+}
+
 func (ui UI) Draw() {
+	w, h := termbox.Size()
+
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	ui.DrawRectangle(0, 0, w - 1, h - 1, '+')
 	ui.PrintCentered(strings.Repeat("Hello, termbox. ", 4))
 	termbox.Flush()
 }

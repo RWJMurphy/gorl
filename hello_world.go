b636a55ccb1c70ddc512b7f5c8bb55c46437df7d
Reed Kraft-Murphy <reed@reedmurphy.net>
2014-10-27 23:33:57 +1100

    Refactoring!

--- hello_world.go
+++ hello_world.go
@@ -1,51 +1,71 @@
 package main
 
 import (
-    "strings"
-    "unicode/utf8"
-    "github.com/nsf/termbox-go"
+	"github.com/nsf/termbox-go"
+	"strings"
+	"unicode/utf8"
 )
 
-func print_at(x, y int, s string) {
-    for i, r := range s {
-        termbox.SetCell(x + i, y, r, termbox.ColorDefault, termbox.ColorDefault)
-    }
+type UI struct{}
+
+func (ui UI) Close() {
+	termbox.Close()
 }
 
-func print_centered(s string) {
-    width, height := termbox.Size()
-    mid_w, mid_h := width / 2, height / 2
-    s_len := utf8.RuneCountInString(s)
-    print_at(mid_w - s_len / 2, mid_h, s)
+func NewUI() (*UI, error) {
+	ui := &UI{}
+	err := termbox.Init()
+	return ui, err
 }
 
-func draw() {
-    termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-    print_centered(strings.Repeat("Hello, termbox. ", 4))
-    termbox.Flush()
+func (ui UI) PrintAt(x, y int, s string) {
+	for i, r := range s {
+		termbox.SetCell(x+i, y, r, termbox.ColorDefault, termbox.ColorDefault)
+	}
+}
+
+func (ui UI) PrintCentered(s string) {
+	width, height := termbox.Size()
+	mid_w, mid_h := width/2, height/2
+	s_len := utf8.RuneCountInString(s)
+	ui.PrintAt(mid_w-s_len/2, mid_h, s)
+}
+
+func (ui UI) Draw() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	ui.PrintCentered(strings.Repeat("Hello, termbox. ", 4))
+	termbox.Flush()
 }
 
 func main() {
-    err := termbox.Init()
-    if err != nil {
-        panic(err)
-    }
-    defer termbox.Close()
+	redraw := false
+	ui, err := NewUI()
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+	ui.Draw()
 
-    draw()
-loop:
-    for {
-        event := termbox.PollEvent()
-        switch event.Type {
-        case termbox.EventKey:
-            switch event.Key {
-            case termbox.KeyCtrlC, termbox.KeyEsc:
-                break loop
-            }
-        case termbox.EventResize:
-            draw()
-        case termbox.EventError:
-            panic(event.Err)
-        }
-    }
+mainLoop:
+	for {
+		redraw = false
+
+		event := termbox.PollEvent()
+		switch event.Type {
+		case termbox.EventKey:
+			switch event.Key {
+			case termbox.KeyCtrlC, termbox.KeyEsc:
+				break mainLoop
+			}
+		case termbox.EventResize:
+			redraw = true
+		case termbox.EventError:
+			panic(event.Err)
+		}
+
+		if redraw {
+			ui.Draw()
+		}
+	}
 }
+

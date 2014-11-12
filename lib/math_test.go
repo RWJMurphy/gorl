e714950dc2f3cc5914653589e8ae8edb4b8457d2
Reed Kraft-Murphy <reed@reedmurphy.net>
2014-11-13 09:09:13 +1100

    Test AbsInt

---
+++ lib/math_test.go
@@ -0,0 +1,27 @@
+package gorl
+
+import (
+	"testing"
+)
+
+func TestIntAbs(t *testing.T) {
+	tests := []struct {
+		in int
+		want uint
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{9999, 9999},
+
+		{-1, 1},
+		{-2, 2},
+		{-9999, 9999},
+	}
+	for _, test := range tests {
+		got := IntAbs(test.in)
+		if got != test.want {
+			t.Errorf("IntAbs(%d) = %d, want %d", test.in, got, test.want)
+		}
+	}
+}
